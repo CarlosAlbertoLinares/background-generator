c1ec6d5f6da8eab3eae4f532882244f3efbe90a5
CarlosAlbertoLinares <51976293+CarlosAlbertoLinares@users.noreply.github.com>
2021-10-07 09:06:08 -0400

    returnning back to old tittle

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pprint
 import os
 
+
 res = requests.get('https://news.ycombinator.com/news')
 res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
